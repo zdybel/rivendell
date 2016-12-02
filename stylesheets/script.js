0278804272f0023bc05f77cb6d5917ed09b5f82b
Brynne Zdybel <bazdybel@gmail.com>
2016-12-01 23:23:19 -0500

    click functions on pics and parallax on page

--- stylesheets/script.js
+++ stylesheets/script.js
@@ -1,3 +1,31 @@
 $(document).ready(function(){
+	
 	console.log("its a miracle");
+
+	firstPic();
+
+	function firstPic(){
+	 	$(".parallax").click(function(){
+	 		console.log('worked');
+	 		$(".parallax").css("background-image","url('http://randomwallpapers.net/rivendell-waterfall-woods-forest-houses-architecture-2560x1600-wallpaper42307.jpg')");
+	 		function switchPic(){
+	 			$(".parallax").click(
+	 				function(){
+	 					console.log('worked again');
+	 					$(".parallax").css("background-image","url('http://i.imgur.com/CNeKLa2.jpg')");
+	 					firstPic();
+	 				});
+	 			};
+	 		switchPic();
+		});
+	 };
+
+	// function switchPic(){
+	//  	$(".parallax").click(
+	//  		function(){
+	//  			console.log('worked again');
+	//  			$(".parallax").attr("bacground-image","url('http://i.imgur.com/CNeKLa2.jpg')");
+	//  			firstPic();
+	//  	});
+	// };
 });
