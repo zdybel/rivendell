6d197be82e3c9567496c70bd725bbb978ac42439
Brynne Zdybel <bazdybel@gmail.com>
2016-12-03 13:49:17 -0500

    main content is grid systemed and added chevron

--- stylesheets/script.js
+++ stylesheets/script.js
@@ -1,3 +1,27 @@
+/* to do on this project:
+		show bootstrap grid knowledge
+			have images with text under
+			have multiple columns
+			have everything centered
+			use wrap
+		use the parralax
+			have the body scroll over the top picture
+		use border radius to make circles
+		use variables in sass
+		use nesting in sass
+		have the articles link to another page with scrollbar pics
+			so make another html page (with allll the links- and make it in terminal
+				under stylesheets)
+			link it to the styles page
+		separate out styles pages in sass separation
+	
+	In general: to practice using bootstrap and sass.
+	
+	*/
+
+
+
+
 $(document).ready(function(){
 	
 	console.log("its a miracle");
@@ -18,7 +42,17 @@ $(document).ready(function(){
 	 			};
 	 		switchPic();
 		});
+
+
+	var widthGimli = $('.gimli').width();
+	$('.gimli').css({'height':widthGimli+'px'});
+	$('.gimli').css({'border-radius':widthGimli+ 'px'});
 	 };
+
+	// var widthAragorn = $('.aragorn').width();
+	// $('.aragorn').css({'height':widthAragorn+'px'});
+	// $('.aragorn').css({'border-radius':widthAragorn+ 'px'});
+	//  };
 
 	// function switchPic(){
 	//  	$(".parallax").click(
